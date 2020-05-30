ecd16734543d940143a48879c82d9c6034c79b93
David <learnitmyway@users.noreply.github.com>
2020-05-30 10:10:37 +0200

    use function expression syntax in App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from 'react'
 import './App.css'
 import Button from './Button'
 
-function App() {
-  return (
-    <div className="App">
-      <Button className="button-bg-color" />
-    </div>
-  )
-}
+const App = () => (
+  <div className="App">
+    <Button className="button-bg-color" />
+  </div>
+)
 
 export default App
