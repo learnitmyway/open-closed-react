7b69062a465633e042c314977db90dba0dbc317b
David <learnitmyway@users.noreply.github.com>
2020-05-30 10:06:48 +0200

    use function expression syntax

--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Button({ className }) {
-  return (
-    <button className={className} type="button">
-      Click Me!
-    </button>
-  )
-}
+const Button = ({ className }) => (
+  <button className={className} type="button">
+    Click Me!
+  </button>
+)
+
+export default Button
